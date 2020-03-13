02fef2732969f792c149c1ec2fcf6246c32e36e7
vinay <vinay.hulgar@gmail.com>
2020-03-13 09:38:38 +0000

    Changed Files

--- CollectorsImpl.java
+++ CollectorsImpl.java
@@ -1,5 +1,5 @@
 import java.util.stream.Collectors;
-public class MyClass {
+public class CollectorsImpl {
     public static void main(String args[]) {
       String str = "OnlineJavaIde";
       System.out.println(str.
--- String/ToArray.java
+++ String/ToArray.java
@@ -11,5 +11,8 @@ public class ToArray {
  
         String[] namesArr1 = names.toArray(new String[names.size()]);       //Before Java 11
         String[] namesArr2 = names.toArray(String[]::new);                  //Since Java 11
+
+        System.out.println(namesArr1);
+        System.out.println(namesArr2);
     }
 }
